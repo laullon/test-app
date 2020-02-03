b4e1d2ca869c5ce15efa87bb67cd2efa8676b2b9
German Laullon <glaullon@vmware.com>
2020-02-03 10:59:15 +0100

    wf

--- go.mod
+++ go.mod
@@ -4,7 +4,16 @@ go 1.13
 
 require (
 	github.com/bmizerany/pat v0.0.0-20140625234703-b8a35001b773 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/opentracing/opentracing-go v1.1.0
 	github.com/pivotal-golang/lager v0.0.0-20150428205713-c88fa6d6c4d2
+	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563
 	github.com/tedsuo/ifrit v0.0.0-20150410161953-65ca48cd8a94
 	github.com/tedsuo/rata v0.0.0-20141210191936-6197c97c67a0
+	github.com/tuvistavie/securerandom v0.0.0-20140719024926-15512123a948 // indirect
+	github.com/wavefronthq/go-metrics-wavefront v1.0.2
+	github.com/wavefronthq/wavefront-opentracing-sdk-go v0.0.0-20191210211418-65cd379fed22
+	github.com/wavefronthq/wavefront-sdk-go v0.9.5
 )
--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
 	"strings"
 	"sync"
 	"time"
 
+	metrics "github.com/rcrowley/go-metrics"
+
 	"github.com/cloudfoundry-samples/test-app/handlers"
 	"github.com/cloudfoundry-samples/test-app/helpers"
 	"github.com/cloudfoundry-samples/test-app/routes"
+	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/pivotal-golang/lager"
 	"github.com/tedsuo/ifrit"
 	"github.com/tedsuo/ifrit/http_server"
 	"github.com/tedsuo/rata"
+	"github.com/wavefronthq/go-metrics-wavefront/reporting"
+	"github.com/wavefronthq/wavefront-opentracing-sdk-go/reporter"
+	"github.com/wavefronthq/wavefront-opentracing-sdk-go/tracer"
+	"github.com/wavefronthq/wavefront-sdk-go/application"
+	"github.com/wavefronthq/wavefront-sdk-go/senders"
 )
+
+type vcapServices struct {
+	WavefrontProxy []struct {
+		Credentials struct {
+			Hostname         string `json:"hostname"`
+			Port             int    `json:"port"`
+			TracingPort      int    `json:"tracingPort"`
+			DistributionPort int    `json:"distributionPort"`
+		} `json:"credentials"`
+	} `json:"wavefront-proxy"`
+}
 
 var message string
 var quiet bool
@@ -37,16 +59,68 @@ func main() {
 
 	logger := lager.NewLogger("test-app")
 	if quiet {
-		logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.INFO))
+		logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
 	} else {
 		logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
 	}
 
 	ports := getServerPorts()
-
-	logger.Info("test-app.starting", lager.Data{"ports": ports})
 	index, err := helpers.FetchIndex()
 	appName := fetchAppName()
+
+	log.Println("VCAP_SERVICES:")
+	log.Println(os.Getenv("VCAP_SERVICES"))
+	var services vcapServices
+
+	var sender senders.Sender
+
+	json.Unmarshal([]byte(os.Getenv("VCAP_SERVICES")), &services)
+	if len(services.WavefrontProxy) > 0 {
+		logger.Info("'Wavefront Proxy' detected, ignoring user input")
+		proxyCfg := &senders.ProxyConfiguration{
+			Host:             services.WavefrontProxy[0].Credentials.Hostname,
+			MetricsPort:      services.WavefrontProxy[0].Credentials.Port,
+			TracingPort:      services.WavefrontProxy[0].Credentials.TracingPort,
+			DistributionPort: services.WavefrontProxy[0].Credentials.DistributionPort,
+		}
+		sender, err = senders.NewProxySender(proxyCfg)
+		if err != nil {
+			logger.Error("creating sender", err)
+		}
+	} else {
+		logger.Info("'Wavefront Proxy' NOT detected, using hardcode values")
+		directCfg := &senders.DirectConfiguration{
+			Server:               "https://nimba.wavefront.com",
+			Token:                "6490a634-ca7d-47c1-bb04-4629f53fc98b",
+			BatchSize:            10000,
+			MaxBufferSize:        50000,
+			FlushIntervalSeconds: 1,
+		}
+
+		sender, err = senders.NewDirectSender(directCfg)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	appTags := application.New(appName, "PCF")
+	wfReporter := reporter.New(sender, appTags, reporter.Source(appName))
+	clReporter := reporter.NewConsoleSpanReporter(appName) //Specify the same source you used for the WavefrontSpanReporter
+	reporter := reporter.NewCompositeSpanReporter(wfReporter, clReporter)
+	tracer := tracer.New(reporter, tracer.WithSampler(tracer.RateSampler{Rate: 1000}))
+	opentracing.InitGlobalTracer(tracer)
+
+	metricsReporter := reporting.NewReporter(
+		sender,
+		appTags,
+		reporting.Source(appName),
+		reporting.Prefix(fmt.Sprintf("pcf.%s.%d", appName, index)),
+		reporting.LogErrors(true),
+		reporting.CustomRegistry(metrics.NewRegistry()),
+	)
+	counter := metricsReporter.GetOrRegisterMetric("Says", metrics.NewCounter(), map[string]string{"t": "v"}).(metrics.Counter)
+
+	logger.Info("test-app.starting", lager.Data{"ports": ports})
 	go func() {
 		t := time.NewTicker(time.Second)
 		for {
@@ -54,7 +128,12 @@ func main() {
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Failed to fetch index: %s\n", err.Error())
 			} else {
+				span := opentracing.StartSpan("Says opertion")
+				span.SetTag("message", message)
+				counter.Inc(1)
 				fmt.Println(fmt.Sprintf("%s. Says %s. on index: %d", appName, message, index))
+				time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+				span.Finish()
 			}
 		}
 	}()
